ab7c6fc3f5e15f18e3fc9c43796982101d4b8720
kainhong <fatsugar@126.com>
2024-04-17 08:29:28 +0800

    调整shell窗口地址

--- src/main/java/cn/mercury/xcode/window/ShellSettings.java
+++ src/main/java/cn/mercury/xcode/window/ShellSettings.java
@@ -5,12 +5,11 @@ import lombok.Data;
 @Data
 public class ShellSettings {
 
-    private String completeURL = "https://dev-wx.tbty.cn/";
+    private String completeURL = "https://dev-wx.demo.cn/";
 
     private Integer maxTokenSize = 1024;
 
     static ShellSettings current = new ShellSettings();
-
     public static ShellSettings getInstance() {
         return current;
     }
